54d60e408e5f448637b6c1d5766a64a271d37df7
Catherine Mitchell <16630303+gynnantonix@users.noreply.github.com>
2021-07-16 13:56:05 -0400

    Moved stylesheets

--- src/components/Switch.js
+++ src/components/Switch.js
@@ -1,22 +1,22 @@
 import React from "react";
-import nouislider from 'nouislider'
+import nouislider from "nouislider";
 import "nouislider/dist/nouislider.css";
-import '../styles/Switch.css';
+import "../styles/controls/Switch.css";
 
 const Switch = ({
   orientation = "vertical",
   start = [0],
   range = {
-    'min': [0, 1],
-    'max': 1
+    min: [0, 1],
+    max: 1,
   },
   // format = wNumb({
   //   decimals: 0
   // }),
-  className = 'switch'
+  className = "switch",
 }) => {
-  const toggle = React.useRef()
-  const [initialized, setInitialized] = React.useState(false)
+  const toggle = React.useRef();
+  const [initialized, setInitialized] = React.useState(false);
 
   React.useEffect(() => {
     if (toggle && toggle.current && !initialized) {
@@ -24,19 +24,19 @@ const Switch = ({
         orientation,
         start,
         range,
-      })
+      });
 
-      toggle.current.addEventListener('click', function (values, handle) {
-        if (values[handle] === '1') {
-          toggle.current.classList.add('off');
+      toggle.current.addEventListener("click", function (values, handle) {
+        if (values[handle] === "1") {
+          toggle.current.classList.add("off");
         } else {
-          toggle.current.classList.remove('off');
+          toggle.current.classList.remove("off");
         }
       });
-      setInitialized(true)
+      setInitialized(true);
     }
-  }, [initialized, orientation, start, range])
-  return <div ref={toggle} className={className} />
-}
+  }, [initialized, orientation, start, range]);
+  return <div ref={toggle} className={className} />;
+};
 
 export default Switch;
