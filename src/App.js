38ce71d0e2fc1690b2d44e2f5b376d3e75bb2fb6
Catherine Mitchell <16630303+gynnantonix@users.noreply.github.com>
2021-07-17 23:54:32 -0400

    prompts for user interaction before loading app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import React from "react";
 import "./App.scss";
-import * as Tone from "tone";
 import { ChannelStrip, TransportControls, Panner3D } from "./components";
 import SynthPad1 from "./instruments/interfaces/SynthPad1";
 import SynthPluck1 from "./instruments/interfaces/SynthPluck1";
 import SynthPluck2 from "./instruments/interfaces/SynthPluck2";
-
 import { BusContextProvider } from "./contexts/BusContext";
 
 function App() {
-  const [started, setStarted] = React.useState(false);
-
-  document.documentElement.addEventListener("mousedown", async () => {
-    if (started) return;
-    setStarted(true);
-    await Tone.start();
-    Tone.Context.lookAhead = "2s";
-  });
-
   return (
     <div className="App">
       <header className="App-header">Kitchen Sink</header>
@@ -32,7 +21,7 @@ function App() {
           </div>
           <SynthPad1 />
           <SynthPluck1 />
-          <SynthPluck2 />
+          {/* <SynthPluck2 /> */}
         </BusContextProvider>
       </main>
     </div>
